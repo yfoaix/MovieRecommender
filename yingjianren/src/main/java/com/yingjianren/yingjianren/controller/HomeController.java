90d3749ecb8ec406d483ada1c212c6096e5df549
xiaofy <1045720796@qq.com>
2020-06-23 09:18:10 +0800

    添加了注释和controller层传输数据的示例

--- yingjianren/src/main/java/com/yingjianren/yingjianren/controller/HomeController.java
+++ yingjianren/src/main/java/com/yingjianren/yingjianren/controller/HomeController.java
@@ -2,14 +2,33 @@ package com.yingjianren.yingjianren.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class HomeController {
+    //根目录
     @GetMapping("/")
     public ModelAndView Index(){
         ModelAndView model = new ModelAndView();
+        //命名为templates里的模板名 index对应 index.html
+        //static文件夹自动映射到根目录
         model.setViewName("index");
+        //在视图模型中加入对象，可以通过th标签显示
+        model.addObject("title", "title");
         return model;
     }
+    //从url中传入数据
+
+    @GetMapping(value="/test/{page}")
+    //从该函数的参数中获得
+    public ModelAndView Test(@PathVariable("page") Integer page) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("test");
+        model.addObject("page", page);
+        return model;
+    }
+    
+
 }
---
+++ yingjianren/src/main/java/com/yingjianren/yingjianren/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.yingjianren.yingjianren.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+}
