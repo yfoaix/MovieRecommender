f9cbcab6b6a5f43506fcc7e5d738997f82ae6d95
xiaofy <1045720796@qq.com>
2020-06-23 09:49:51 +0800

    添加了rest和ajax get示例

--- yingjianren/src/main/java/com/yingjianren/yingjianren/controller/TestController.java
+++ yingjianren/src/main/java/com/yingjianren/yingjianren/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.yingjianren.yingjianren.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//使用restcontroller 就可以不使用 @ResponseBody 来防止跳转到链接
 @RestController
 public class TestController {
+    @RequestMapping(value = "/ajaxtest/{page}")
     
+    public String GetPage(@PathVariable("page") Integer page){
+        return "{\"movie\":\"1\"}";
+    }
 }
