c60ab1b0bf5c8f307b7e0707630427b8e269175e
JMwashington <553871972@qq.com>
2020-07-03 10:26:45 +0800

    又修改了表结构

--- yingjianren/src/main/java/com/yingjianren/yingjianren/entity/User.java
+++ yingjianren/src/main/java/com/yingjianren/yingjianren/entity/User.java
@@ -2,16 +2,22 @@ package com.yingjianren.yingjianren.entity;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @TableGenerator(name = "user_gen",
+            table="user_generator",
+            pkColumnName="gen_id",
+            valueColumnName="gen_value",
+            pkColumnValue="CUSTOMER_PK",
+            allocationSize=1,
+            initialValue=227207
+    )
+
+    @GeneratedValue(generator = "user_gen")
     private Long userId;//id,自增主键
 
     private String userName;//用户名
@@ -20,12 +26,14 @@ public class User {
 
     private String userPwd;//用户密码
 
+    @Column(nullable = false)
     private boolean authentication;//是否是影荐人
 
     private String biography;//个人简介
 
     private String imgUrl;//头像
 
+    @Column(nullable = false)
     private boolean confirm;//邮箱是否验证
 
 }
