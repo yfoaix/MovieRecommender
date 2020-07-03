9ee006b0afca91e169ac53217bc52556be622cd2
FayeKe <kyqq990304@163.com>
2020-07-03 21:24:49 +0800

    no message

--- yingjianren/src/main/java/com/yingjianren/yingjianren/controller/HomeController.java
+++ yingjianren/src/main/java/com/yingjianren/yingjianren/controller/HomeController.java
@@ -10,11 +10,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
     //根目录
     @GetMapping("/")
-    public ModelAndView layout(){
+    public ModelAndView index(){
         ModelAndView model = new ModelAndView();
         //命名为templates里的模板名 index对应 index.html
         //static文件夹自动映射到根目录
-        model.setViewName("layout");
+        model.setViewName("index");
         //在视图模型中加入对象，可以通过th标签显示
         model.addObject("title", "title");
         return model;
