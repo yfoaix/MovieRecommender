2ed6c5784268f18fea9b7e15a1afb84e7cfbef15
yue912 <1345093538@qq.com>
2020-07-04 11:21:30 +0800

    ui7_4

--- yingjianren/src/main/java/com/yingjianren/yingjianren/controller/HomeController.java
+++ yingjianren/src/main/java/com/yingjianren/yingjianren/controller/HomeController.java
@@ -5,30 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 public class HomeController {
-    //根目录
+    // 根目录
     @GetMapping("/")
-    public ModelAndView layout(){
+    public ModelAndView search() {
         ModelAndView model = new ModelAndView();
-        //命名为templates里的模板名 index对应 index.html
-        //static文件夹自动映射到根目录
-        model.setViewName("layout");
-        //在视图模型中加入对象，可以通过th标签显示
+        // 命名为templates里的模板名 index对应 index.html
+        // static文件夹自动映射到根目录
+        model.setViewName("list");
+        // 在视图模型中加入对象，可以通过th标签显示
         model.addObject("title", "title");
         return model;
     }
-    //从url中传入数据
+    // 从url中传入数据
 
-    @GetMapping(value="/test/{page}")
-    //从该函数的参数中获得
+    @GetMapping(value = "/test/{page}")
+    // 从该函数的参数中获得
     public ModelAndView Test(@PathVariable("page") Integer page) {
         ModelAndView model = new ModelAndView();
         model.setViewName("test");
         model.addObject("page", page);
         return model;
     }
-    
 
 }
